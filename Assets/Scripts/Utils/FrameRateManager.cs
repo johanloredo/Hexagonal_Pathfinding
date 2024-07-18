15e972c5007a4ac83dde044becd12ca663cbdf84
Johan Pablo Loredo Pineda <johan.loredopa@udlap.mx>
2024-07-18 09:52:20 -0600

    Updated windows build

--- Assets/Scripts/Utils/FrameRateManager.cs
+++ Assets/Scripts/Utils/FrameRateManager.cs
@@ -5,7 +5,7 @@ public class FrameRateManager : MonoBehaviour
 {
     [Header("Frame Settings")]
     int maxRate = 9999;
-    public float TargetFrameRate = 60.0f;
+    public float targetFrameRate = 60.0f;
     float currentFrameTime;
 
     void Awake()
@@ -21,11 +21,14 @@ public class FrameRateManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForEndOfFrame();
-            currentFrameTime += 1.0f / TargetFrameRate;
+
+            currentFrameTime += 1.0f / targetFrameRate;
             var t = Time.realtimeSinceStartup;
             var sleepTime = currentFrameTime - t - 0.01f;
+
             if (sleepTime > 0)
                 Thread.Sleep((int)(sleepTime * 1000));
+
             while (t < currentFrameTime)
                 t = Time.realtimeSinceStartup;
         }
